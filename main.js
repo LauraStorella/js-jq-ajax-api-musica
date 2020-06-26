35d2d26624ab86ee3a626b64c711010e4e81abf9
Laura <lstorella10@gmail.com>
2020-06-26 15:53:09 +0200

    Step #3 : inserito logica

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
+	// RACCOLTA ALBUM MUSICALI
+	/* 
+	Step #1 - Attraverso una chiamata ajax all’Api di boolean avremo a 
+	disposizione una decina di dischi musicali. 
+	
+	Step #2 - Servendoci di handlebars stampiamo tutto a schermo. 
+	In questo momento non è importante la parte graﬁca.
+	
+	Step #3 - Bonus: Creare una select con i seguenti generi: pop, rock, 
+	metal e jazz. In base a cosa scegliamo nella select vedremo i 
+	corrispondenti cd. */
+
+
+	// STEP #1
 	// Chiamata Ajax per comunicare con API
 	$.ajax(
 		{
@@ -10,6 +24,26 @@ $(document).ready(function() {
 
 				// Chiamo funzione per stampa cover album
 				printCds(cds)
+
+				// Step #3 : SELECT - inizio
+				// Creo evento click sulla select
+				$('option').click( function() {
+					// alert('test');
+
+					// Leggo il valore della select
+					var userSelection = $(this).val();
+					console.log(userSelection);
+
+					// Se il valore è diverso da all
+					//  ---> quindi l'utente ha applicato un  filtro
+					if (userSelection != all) {
+						// nascondo tutti i dischi (aggiungo classe .hide)
+						// e visualizzo quelli corrispondenti alla scelta utente (aggiungo classe .show)
+					} else {
+						// visualizzo tutti gli album
+					}
+				});
+				// SELECT - fine
 			},
 
 			error: function () {
@@ -22,6 +56,8 @@ $(document).ready(function() {
 
 
 	// ----------------------------- FUNCTION -----------------------------
+	
+	// STEP #2
 	//  Creo funzione per inserire gli album in html (.cds-container)
 	//  ---> arrayCds : contiene elenco cd da stampare
   //  ---> utilizzo handlebar per popolare .cds-container con cover album
